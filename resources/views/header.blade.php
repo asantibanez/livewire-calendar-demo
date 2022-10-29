197cad3970a5d4f682242311437ff65717ea413c
Beej <howdu@users.noreply.github.com>
2022-10-29 13:35:10 +0100

    Remove typos

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,7 +8,6 @@
                 wire:click.stop="goToPreviousMonth"
                 type="button"
                 class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                <
             </button>
             <button
                 wire:click.stop="goToCurrentMonth"
@@ -20,7 +19,6 @@
                 wire:click.stop="goToNextMonth"
                 type="button"
                 class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                >
             </button>
             <button
                 type="button"
